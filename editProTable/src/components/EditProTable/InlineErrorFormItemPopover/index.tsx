773ddd7ac1cfbd88786ffd1846b678d803a077a3
chenbo <3212847512@qq.com>
2025-03-05 08:32:11 +0800

    feat(editProTable): 添加校验时机逻辑

--- editProTable/src/components/EditProTable/InlineErrorFormItemPopover/index.tsx
+++ editProTable/src/components/EditProTable/InlineErrorFormItemPopover/index.tsx
@@ -1,55 +1,68 @@
-import { Form, Popover } from "antd";
-import { NamePath } from "antd/es/form/interface";
-import { FC, useState } from "react";
+import { Form, Popover } from 'antd'
+import { NamePath } from 'antd/es/form/interface'
+import { FC, useState } from 'react'
 
 interface InlineErrorFormItemPopoverProps {
-  fieldName: NamePath;
+  fieldName: NamePath
   // 错误信息展示类型
-  errorType: "popover" | undefined;
-  children: React.ReactNode;
+  errorType: 'popover' | undefined
+  children: React.ReactNode
 }
 /**
  * @description 行内错误信息展示
+ *  1. 获取errors 错误信息；
+ *  2. 如果有错误就展示，没有就不需要展示；
+ *
+ *
+ *
  */
 const InlineErrorFormItemPopover: FC<InlineErrorFormItemPopoverProps> = ({
   children,
   fieldName,
   //   errorType,
 }) => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false)
+  const [refresh, setRefresh] = useState(0)
+  const form = Form.useFormInstance()
+
+  console.log(form.getFieldError(fieldName))
 
   return (
     <Form.Item
       noStyle
       shouldUpdate={(prevValues, curValues) => {
         // 监听字段值变化
+        setTimeout(() => {
+          setRefresh(Math.random())
+          // console.log('refresh')
+        }, 0)
+
         return (
           prevValues[fieldName as string] !== curValues[fieldName as string]
-        );
+        )
       }}
     >
+      {/*  */}
       {(form) => {
-        const errors = form.getFieldError(fieldName);
-        const hasError = errors.length > 0;
+        const errors = form.getFieldError(fieldName)
+        const hasError = errors.length > 0
 
         return (
           <Popover
             open={hasError && open}
             onOpenChange={setOpen}
             content={
-              <span style={{ color: "#ff4d4f" }}>{errors.join(", ")}</span>
+              <span style={{ color: '#ff4d4f' }}>{errors.join(', ')}</span>
             }
-            trigger={["click"]}
             placement="topLeft"
+            trigger={['click']}
           >
-            <div onFocus={() => setOpen(true)} onBlur={() => setOpen(false)}>
-              {children}
-            </div>
+            <div>{children}</div>
           </Popover>
-        );
+        )
       }}
     </Form.Item>
-  );
-};
+  )
+}
 
-export default InlineErrorFormItemPopover;
+export default InlineErrorFormItemPopover
